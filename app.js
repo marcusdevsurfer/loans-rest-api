ab4beb76e4649cdf6850a49303ffb35ce697fb18
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-09 13:25:18 -0600

    code clean and db initialization

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const cors = require("cors")
+const express = require('express');
+const cors = require('cors')
+const sqlite3 = require('sqlite3').verbose();
 const { getPaymentsByLoanId } = require('./data/data.js');
+
+const db = new sqlite3.Database('loansdb.sql');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS payments(date TEXT)');
+})
+db.close()
 
 app.use(cors())
 
