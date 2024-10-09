25ce6a69c6c0d325976fd5e50d19adc572c8ebb4
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-09 00:11:12 -0600

    refact: cors add

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors'
+
 const app = express()
 const PORT = process.env.PORT || 3000;
 import { getPaymentsByLoanId } from './data/data.js';
+
+app.use(cors())
 
 app.get('/api/users/:id/payments', function (req, res) {
     res.send(getPaymentsByLoanId(req.params.id))
