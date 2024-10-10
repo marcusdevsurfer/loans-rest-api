558368c393c4f550749e20c2504107ef6bebd9e1
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-09 18:44:36 -0600

    endpoint code clean in progress

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ const express = require('express');
 const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose();
 const { getPaymentsByLoanId } = require('./data/data.js');
+const { getPayments, addPayment } = require('./src/service/paymentService.js')
 
 const db = new sqlite3.Database('loansdb.sql');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS payments(date TEXT)');
+    db.run('CREATE TABLE IF NOT EXISTS payments(date TEXT, amount INT, loanId INT)');
 })
 db.close()
-
 app.use(cors())
 
-app.get('/api/users/:id/payments', function (req, res) {
+app.get("/api/loans/:id/payments", (req, res) => {
     res.send(getPaymentsByLoanId(req.params.id))
+})
+
+app.post("/api/payments", (req, res) => {
+    res.send({ name: "Marcus" })
 })
 
 app.listen(PORT, () => {
