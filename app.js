bf112013466f7a12f5163d1d8c676b8d87851b4c
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-09 19:26:59 -0600

    refact: addition of post endpoint to create payments

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose();
-const { getPaymentsByLoanId } = require('./data/data.js');
-const { getPayments, addPayment } = require('./src/service/paymentService.js')
+const { getPaymentsByLoanId, addPayment } = require('./src/service/paymentService.js')
 
-const db = new sqlite3.Database('loansdb.sql');
+const db = new sqlite3.Database('loansdb.sqlite');
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 db.serialize(() => {
     db.run('CREATE TABLE IF NOT EXISTS payments(date TEXT, amount INT, loanId INT)');
 })
-db.close()
+
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/api/loans/:id/payments", (req, res) => {
     res.send(getPaymentsByLoanId(req.params.id))
 })
-
 app.post("/api/payments", (req, res) => {
-    res.send({ name: "Marcus" })
+    const date = req.body.date
+    const amount = req.body.amount
+    const loanId = req.body.loanId
+    addPayment(res, date, amount, loanId)
 })
 
 app.listen(PORT, () => {
