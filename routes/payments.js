0acf6880026e479702cfa04ac8cf9a7ebf823115
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-15 14:19:14 -0600

    add find payment by id method

--- routes/payments.js
+++ routes/payments.js
@@ -4,11 +4,10 @@ import Payment from '../models/Payment.js'
 const router = express.Router()
 
 
-// Crear un nuevo item
-router.post('/', async (req, res) => {
-    const { amount } = req.body;
+// Crear un nuevo Payment
+router.post('/save', async (req, res) => {
     try {
-        const newPayment = new Payment({ amount });
+        const newPayment = await Payment.create(req.body);
         await newPayment.save();
         res.status(201).json(newPayment);
     } catch (error) {
@@ -20,10 +19,22 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const payments = await Payment.find();
-        res.json(payments);
+        res.status(200).json(payments);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const payment = await Payment.findById(id)
+        res.status(200).json(payment)
+    } catch {
+        (e) => {
+            res.status(500).json({ message: e.message })
+        }
+    }
+})
 
 export default router
